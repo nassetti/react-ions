a4d348fcc30f27417c3e0563171daca9b7c99500
nassetti <nassettimax@gmail.com>
2022-12-06 21:38:44 +0000

    Add registration form.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import styles from "./App.module.css"
-import NavBar from './components/NavBar';
-import Container from "react-bootstrap/Container"
-import {Route, Switch} from "react-router-dom"
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import Register from "./pages/auth/Register";
 
 function App() {
   return (
@@ -11,8 +13,21 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home</h1>} />
           <Route exact path="/sign-in" render={() => <h1>Sign-In</h1>} />
-          <Route exact path="/register" render={() => <h1>Register</h1>} />
-          <Route exact path="/post/create" render={() => <h1>Create Post</h1>} />
+          <Route
+            exact
+            path="/register"
+            render={() => (
+              <h1>
+                {" "}
+                <Register />
+              </h1>
+            )}
+          />
+          <Route
+            exact
+            path="/post/create"
+            render={() => <h1>Create Post</h1>}
+          />
           <Route render={() => <h1>PAGE NOT FOUND</h1>} />
         </Switch>
       </Container>
---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://react-ions.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+
+
